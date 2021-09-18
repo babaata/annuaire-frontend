4cd81da4e3f52960593288548ab9ba626a71077f
AbdoulGoudoussy <goudoussy2000@gmail.com>
2021-09-18 14:28:11 +0100

    navbar namespace change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Banner from "./components/body/HomeScreen/Banner/Banner";
 import Competences from "./components/body/HomeScreen/CompetenceSection/Competences";
 import Inscription from "./components/body/HomeScreen/InscriptionSection/Inscription";
-import Menubar from "./components/body/HomeScreen/NavBar/Menubar";
+import Menubar from "./components/body/HomeScreen/Navbar/Menubar";
 import PopularProfil from "./components/body/HomeScreen/profils/PopularProfil";
 import StatistiqueSection from "./components/body/HomeScreen/StatistiqueSection/StatistiqueSection";
 import Footer from "./components/footer/Footer";
