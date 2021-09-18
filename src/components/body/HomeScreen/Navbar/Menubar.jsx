9200674de12c418221975ae915971382d0658544
AbdoulGoudoussy <goudoussy2000@gmail.com>
2021-09-18 13:20:34 +0100

    navbar udpated

--- src/components/body/HomeScreen/Navbar/Menubar.jsx
+++ src/components/body/HomeScreen/Navbar/Menubar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import './Menubar.css';
+import React from "react";
+import "./Menubar.css";
 export default function Menubar() {
-    return (
-        <div className="menubar">
-            <div className="logo">
-                <img src="./images/logo.png" alt="logo"/>
-            </div>
-            <div className="link_button">
-                <ul>
-                    <li>
-                        <a href="/">Connexion</a>
-                    </li>
-                    <li>
-                        <a href="/" className="active">Inscription</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className="menubar">
+      <div className="logo">
+        <img src="./images/logo.png" alt="logo" />
+      </div>
+      <div className="link_button">
+        <ul>
+          <li>
+            <a href="/">Connexion</a>
+          </li>
+          <li>
+            <a href="/" className="active">
+              Inscription
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
