b9316bc79c3801f69e980d0a1652ac0b4d28be94
AbdoulGoudoussy <goudoussy2000@gmail.com>
2021-09-18 09:59:43 +0100

    body and footer added without the navbar and banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Competences from './components/body/HomeScreen/CompetenceSection/Competences';
+import Inscription from './components/body/HomeScreen/InscriptionSection/Inscription';
+import PopularProfil from './components/body/HomeScreen/profils/PopularProfil';
+import StatistiqueSection from './components/body/HomeScreen/StatistiqueSection/StatistiqueSection';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <PopularProfil/>
+      <Inscription/>
+      <Competences/>
+      <StatistiqueSection/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__content__top">
+        <div className="social__media">
+          <a href="#">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+        <div className="news__letter">
+          <form>
+            <div className="news__letter__form">
+              <i className="fas fa-envelope"></i>
+              <input type="text" placeholder="Newsletter" />
+              <button>
+                <i className="fas fa-paper-plane"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <hr />
+      <div className="footer__content__bottom">
+        <span>Copyright</span>
+        <ul>
+          <li>
+            <a href="#">A propos</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
