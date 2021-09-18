7399e68072370401d018bbb2201f84397669af63
ibrah25.b@gmail.com <ibrah25.b@gmail.com>
2021-09-18 10:43:38 +0000

    hearder added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Banner from './components/body/HomeScreen/Banner/Banner';
+import Menubar from './components/body/HomeScreen/NavBar/Menubar';
+
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Menubar />
+      <Banner />
     </div>
   );
 }
