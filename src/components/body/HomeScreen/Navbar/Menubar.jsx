6e2d821fa4987b27fd94a5328e8bfb32d2c04216
AbdoulGoudoussy <goudoussy2000@gmail.com>
2021-09-19 08:49:18 +0100

    detail Section head done

--- src/components/body/HomeScreen/Navbar/Menubar.jsx
+++ src/components/body/HomeScreen/Navbar/Menubar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./Menubar.css";
 import Login from "../../../auth/Login/login.component";
 import Register from "../../../auth/Register/register.component";
+import { Link } from "react-router-dom";
 
 export default function Menubar() {
   return (
     <div className="menubar">
       <div className="logo">
-        <img src="./images/logo.png" alt="logo" />
+        <Link to="/">
+          <img src="./images/logo.png" alt="logo" />
+        </Link>
       </div>
       <div className="link_button">
         <ul>
