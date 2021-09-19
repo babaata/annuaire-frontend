983a087c9861e47cb82e43c7051b9d4201ab51d4
ibrah25.b@gmail.com <ibrah25.b@gmail.com>
2021-09-19 08:27:13 +0000

    Detail section experience component added

--- src/components/body/DetailSream/ExperienceComponent.jsx
+++ src/components/body/DetailSream/ExperienceComponent.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function ExperienceComponent() {
     return (
-        <div>
-            
+        <div className="experienceComponent">
+            <h2 className="header_title tile">Expériences / Réalisations</h2>
+            <div className="experiencecard">
+            <div className="title_years">
+                <p className="first_page_title">Lead designer chez <span className="see_more" >Google</span></p>
+                <p>2018 - à nos jours</p>
+            </div>
+            <div className="see_more">
+                <a href="/"><i class="fas fa-chevron-right"></i>En savoir plus</a>
+            </div>
+            </div>
         </div>
     )
 }
