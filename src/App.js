8da46b7b89ff888dd828f669bc35920ab13d37c6
ibrah25.b@gmail.com <ibrah25.b@gmail.com>
2021-09-19 10:11:13 +0000

    Detail section upadated two

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import SearchPage from "./pages/SearchPage";
+import Home from "./Pages/Home";
+import SearchPage from "./Pages/SearchPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ProfilList from "./pages/ProfilList";
-import ProfiDetails from "./pages/ProfilDetails";
+import ProfilList from "./Pages/ProfilList";
+import ProfiDetails from "./Pages/ProfilDetails";
 function App() {
   return (
     <Router>
