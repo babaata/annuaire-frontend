8dfc6deaeef7f412fe66104798d7ce0bc1a5add2
Barry550 <mamadoubarry6059@gmail.com>
2021-09-19 11:48:24 +0000

    redux

---
+++ src/redux/reducers/authReducer.jsx
@@ -0,0 +1,17 @@
+import { AUTH } from "../actions/authAction"
+
+
+const initState = {
+
+}
+
+const authReducer = (state = initState, action) => {
+    switch (action.type) {
+      case AUTH: return action.payload
+    
+      default: return state
+       
+    }
+}
+
+export default authReducer
