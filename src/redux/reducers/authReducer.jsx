aa3504a81d1a57c83ef4abbd7419bf5f07efd586
ib3g <i.barry3g@gmail.com>
2021-09-20 14:19:24 +0100

    WIP merge auth from Barry

--- src/redux/reducers/authReducer.jsx
+++ src/redux/reducers/authReducer.jsx
@@ -1,14 +1,15 @@
 import { AUTH, LOADING } from "../actions/authAction";
 
-const initState = {isLoading: false
-};
+const initState = { isLoading: false };
 
 const authReducer = (state = initState, action) => {
   switch (action.type) {
     case AUTH:
       return action.payload;
 
-    case LOADING: return {...state, isLoading: true}default:
+    case LOADING:
+      return { ...state, isLoading: true };
+    default:
       return state;
   }
 };
