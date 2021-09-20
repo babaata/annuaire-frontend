b26139df570353c375fc19f94b64c5c50b5e4046
ib3g <i.barry3g@gmail.com>
2021-09-20 09:42:19 +0100

    FIX - Footer responsive and add responsive for ProfilList

--- src/Pages/ProfilList.jsx
+++ src/Pages/ProfilList.jsx
@@ -12,14 +12,32 @@ function ProfilList() {
       <Menubar />
       <Inscription margin="2%" text="Tous les profils" />
       <div className="card__section items">
-        <CardProfile image="./images/abdoul.jpeg" color="orange" />
-        <CardProfile image="./images/model.jpeg" color="#009B95" />
-        <CardProfile image="./images/souare.jpeg" color="#ED2F24" />
-        <CardProfile image="./images/younouss.jpeg" color="#326FB4" />
-        <CardProfile image="./images/kabaci.jpeg" color="#AF3862" />
-        <CardProfile image="./images/woman.jpeg" color="#ED2F24" />
-        <CardProfile image="./images/kabaci.jpeg" color="#FBB017" />
-        <CardProfile image="./images/woman.jpeg" color="#1CA5A0" />
+        <div className="row justify-content-center">
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/abdoul.jpeg" color="orange" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/model.jpeg" color="#009B95" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/souare.jpeg" color="#ED2F24" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/younouss.jpeg" color="#326FB4" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/kabaci.jpeg" color="#AF3862" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/woman.jpeg" color="#ED2F24" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/kabaci.jpeg" color="#FBB017" />
+          </div>
+          <div className="col-6 col-lg-3">
+            <CardProfile image="./images/woman.jpeg" color="#1CA5A0" />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
