f7daaeb22add682a3e3855c9a87af5c960337cd6
bahaboubackr <bahaboubackr@gmail.com>
2021-09-26 21:51:02 +0000

    message No profile found

--- src/components/body/SearchScreen/SearchContainrRight.jsx
+++ src/components/body/SearchScreen/SearchContainrRight.jsx
@@ -3,10 +3,11 @@ import SearchContainCard from "./SearchContainCard";
 import './SearchContainRight.css'
 
 function SearchContainrRight(props) {
+  
   return (
     <div className="search__contain__right">
       {props?.profils?.map((p) => (
-        <SearchContainCard profil={p} key={p}/>
+        <SearchContainCard profil={p} key={p.id_utilisateur}/>
       ))}
     </div>
   );
