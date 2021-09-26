dc090571bad5e385d1ef99955af8ea286097a8da
bahaboubackr <bahaboubackr@gmail.com>
2021-09-26 23:21:37 +0000

    Les statistiques

--- src/components/body/HomeScreen/StatistiqueSection/StatistiqueSection.jsx
+++ src/components/body/HomeScreen/StatistiqueSection/StatistiqueSection.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import StatistiqueCard from "./StatistiqueCard";
 import "./StatistiqueSection.css";
 
-function StatistiqueSection() {
+function StatistiqueSection(props) {
   return (
     <div className="statistique container my-3">
       <div className="row mb-5">
         <div className="col-12 col-lg-6 statistique-column-container py-1">
-          <StatistiqueCard number="100 000" name="Utilisateurs" img="./images/Ellipse 33.svg" />
-          <StatistiqueCard number="200" name="Profession" img="./images/Ellipse 34.svg" />
+          <StatistiqueCard number={props.statistiques?.utilisateur} name="Utilisateurs" img="./images/Ellipse 33.svg" />
+          <StatistiqueCard number={props.statistiques?.profession} name="Profession" img="./images/Ellipse 34.svg" />
         </div>
         <div className="col-12 col-lg-6 statistique-column-container py-1">
-          <StatistiqueCard number="2000" name="Compétences" img="./images/Ellipse 35.svg"  />
+          <StatistiqueCard number={props.statistiques?.competence} name="Compétences" img="./images/Ellipse 35.svg"  />
           <StatistiqueCard
-            number="100"
+            number={props.statistiques?.pays}
             name="Pays de résidence"
             img="./images/Ellipse 36.svg"
           />
