96c7adb893be59568ebb11ee6476ba2b1aa499f5
Barry550 <mamadoubarry6059@gmail.com>
2021-09-26 19:12:09 +0000

    fix: pagination

--- src/Pages/ProfilList.jsx
+++ src/Pages/ProfilList.jsx
@@ -1,171 +1,78 @@
-// import React, { useEffect, useState }from "react";
-// import Inscription from "../components/body/HomeScreen/InscriptionSection/Inscription";
-// import Menubar from "../components/body/HomeScreen/Navbar/Menubar";
-// import CardProfile from "../components/body/HomeScreen/profils/CardProfile";
-// import Footer from "../components/footer/Footer";
-// import "../components/body/HomeScreen/profils/PopularProfil.css";
-// import "./ProfilList.css";
-// import {Box, CssBaseline, Container, Typography, Grid, Card, CardContent} from '@material-ui/core'
-// import Pagination from '@material-ui/lab/Pagination'
-// import { getDataAPI } from '../utils/fetchData';
-// import axios from "axios";
-
-// function ProfilList(props) {
-
-//   const [users, setUsers] = useState()
-//   const [page, setPage] = useState()
-//   const [total, setTotal] = useState()
-//   const [size, setSize] = useState(2)
-//   const [loading, setLoading] = useState(true)
-
-//   // const getProfil = async (id) => {
-//   //   const res = await axios.get(`https://babaata.eviltech.org/api/users/pagination?size=${size}&page=${1}`)
-//   //   setUsers(res.data?.users)
-//   //   setTotal(11)
-//   //   setLoading(false)
-//   // }
-
-//   useEffect(async()=>{
-
-//     if (!users?.data) {
-//       if (props.location.profile) {
-//         setUsers(props.location.profile)
-//       } else {
-//         const res = await axios.get(`https://babaata.eviltech.org/api/users/pagination?size=${size}&page=${}`)
-//         console.log(res?.data);
-//         setUsers(res?.data?.users?.data)
-//         setTotal(11)
-//         setLoading(false)
-//       }
-//     }
-
-//   },[page])
-
-//   // if (!users?.data) {
-//   //   if (props.location.profile) {
-//   //     setUsers(props.location.profile)
-//   //   } else {
-//   //     getProfil(props.match.params.profilsId)
-//   //   }
-//   // }
-//   console.log(users)
-//   console.log(total);
-
-//   const pageCout = Math.ceil(loading ? <p>...loading</p> : 10 / size); 
-
-//   return (
-//     <div className="profil_list">
-//       <Typography>
-//       <Menubar />
-//       <Inscription margin="2%" text="Tous les profils" />
-//       <div className="card__section items">
-//         <div className="row justify-content-center">
-//           {!users ? <div className="d-flex justify-content-center"><i className="fa fa-spinner fa-spin fa-2x"></i></div> : ''} 
-//           {
-//             users?.map((u) => (
-//               <div className="col-6 col-lg-3" key={u.id_utilisateur}>
-//                 <CardProfile image="./images/souare.jpeg" color="#326FB4" profile={u}/>
-//               </div>
-//             ))
-//           }
-          
-//         </div>
-//       </div>
-//       </Typography>
-//       <CssBaseline />
-//       <Container component={Box} py={3}>
-//         <Pagination 
-//         count={pageCout}                                         
-//         color="secondary"
-//         variant="outlined"
-//         showFirstButton={true}
-//         showLastButton={true}
-//         onClick={(e, value)=>setPage(parseInt(value))}
-//         />
-//       </Container>
-
-//         <Footer />
-//     </div>
-//   );
-// }
-
-// export default ProfilList;
-
-
-
-import React, { useState } from "react";
+import React, { useEffect, useState }from "react";
 import Inscription from "../components/body/HomeScreen/InscriptionSection/Inscription";
 import Menubar from "../components/body/HomeScreen/Navbar/Menubar";
 import CardProfile from "../components/body/HomeScreen/profils/CardProfile";
 import Footer from "../components/footer/Footer";
 import "../components/body/HomeScreen/profils/PopularProfil.css";
 import "./ProfilList.css";
-import ReactPaginate from 'react-paginate'
+import {Box, CssBaseline, Container, Typography} from '@material-ui/core'
+import Pagination from '@material-ui/lab/Pagination'
 import { getDataAPI } from '../utils/fetchData';
 
 function ProfilList(props) {
-  const [users, setUsers] = useState();
 
-  const getProfil = async (id) => {
-    const res = await getDataAPI("users");
-    setUsers(res.data?.users);
-  };
+  const [users, setUsers] = useState()
+  const [page, setPage] = useState(1)
+  const [total, setTotal] = useState()
+  const [size, setSize] = useState(8)
 
-  if (!users){
-    if (props.location.profile) {
-      setUsers(props.location.profile);
-    } else {
-      getProfil(props.match.params.profilsId);
+  useEffect(()=>{
+    const getProfil = async (id) => {
+      const res = await getDataAPI(`users/pagination?size=${size}&page=${page}`)
+      setUsers(res.data?.users?.data)
+      setTotal(res.data?.users?.total)
     }
-  }
+    
+        if (props.location.profile) {
+          setUsers(props.location.profile)
+        } else {
+          getProfil(props.match.params.profilsId)
+        }  
+  },[page])
 
-  const [pageNumber, setPageNumber] = useState(0)
-
-  const usersPerPage = 8;
-  const pagesVisited = pageNumber * usersPerPage;
 
   
-  const pageCout = Math.ceil(users?.length / usersPerPage); 
-
-  const changePage = ({selected}) => {
-    setPageNumber(selected)
-  }
+  const pageCout = Math.ceil(total / size); 
+ 
 
   return (
     <div className="profil_list">
+      <Typography>
       <Menubar />
       <Inscription margin="2%" text="Tous les profils" />
       <div className="card__section items">
         <div className="row justify-content-center">
           {!users ? <div className="d-flex justify-content-center"><i className="fa fa-spinner fa-spin fa-2x"></i></div> : ''} 
           {
-            users
-            ?.slice(pagesVisited, pagesVisited + usersPerPage)
-            ?.map((u) => (
+            users?.map((u) => (
               <div className="col-6 col-lg-3" key={u.id_utilisateur}>
-                <CardProfile image="./images/souare.jpeg" color="#326FB4" profile={u}/>
+                <CardProfile image={u.url_photo !== "" ? u.url_photo : "./images/loading.gif"} color="#326FB4" profile={u}/>
               </div>
             ))
           }
           
         </div>
       </div>
-        <div className="d-flex justify-content-center">
-        <ReactPaginate 
-          previousLabel={"Previous"}
-          nextLabel={"Next"}  
-          pageCount={pageCout}
-          onPageChange={changePage}
-          containerClassName={'paginationBttns'}
-          disabledClassName={"paginationDisabled"}
-          activeClassName={"paginationActive"}
-          onPageActive={"active"}
-          disableInitialCallback={"disable"}
-      />
-        </div>
-      <Footer />
+      </Typography>
+      <CssBaseline />
+       
+        <Container className="d-flex justify-content-center" component={Box} py={3}>
+        <Pagination 
+        count={pageCout}                                         
+        color="secondary"
+        variant="outlined"
+        showFirstButton={true}
+        showLastButton={true}
+        onChange={(e, value)=> setPage(value)}
+        />
+      </Container>
+     
+        <Footer />
     </div>
   );
 }
 
-export default ProfilList;
+export default ProfilList
+
+
+
