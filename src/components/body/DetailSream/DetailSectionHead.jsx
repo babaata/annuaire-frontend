a44a386d1db96749ee6736fe5a6c98e4550e88fd
ibrah25.b@gmail.com <ibrah25.b@gmail.com>
2021-09-28 16:29:20 +0000

    fix : some error

--- src/components/body/DetailSream/DetailSectionHead.jsx
+++ src/components/body/DetailSream/DetailSectionHead.jsx
@@ -16,7 +16,7 @@ function DetailSectionHead(props) {
             <h5>{`${props.profile?.prenom.charAt(0).toUpperCase()}${props.profile?.prenom.slice(1)} ${props.profile?.nom}`}</h5>
             <p className="fonction">{props.profile?.profil?.titre}</p>
               <span><i className="fas fa-map-marker-alt"></i>Conakry</span>
-              <p>{props.profile?.profil?.resume} .</p>
+              <p>{props.profile?.profil?.resume}</p>
           </div>
         </div>
       </div>
